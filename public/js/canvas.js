c14c6a1ddae37c089651c2cbf7b6474300bd616e
Aurélien Ribon <aurelien.ribon@vasco.com>
2014-01-13 17:52:00 +0100

    Add circle shape support

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -7,12 +7,13 @@
 var Canvas = function(w, h) {
     var that = this;
 
-    this.shape = {points: []};
+    this.shape = null;
 
     this.stage = new Kinetic.Stage({container: 'canvas', width: w, height: h});
     this.backLayer = new Kinetic.Layer();
     this.linesLayer = new Kinetic.Layer();
     this.pointsLayer = new Kinetic.Layer();
+    this.frontLayer = new Kinetic.Layer();
 
     var backConfig = _.merge({width: w, height: h}, Canvas.BackStyle);
     this.backLayer.add(new Kinetic.Rect(backConfig));
@@ -20,130 +21,153 @@ var Canvas = function(w, h) {
     this.stage.add(this.backLayer);
     this.stage.add(this.linesLayer);
     this.stage.add(this.pointsLayer);
+    this.stage.add(this.frontLayer);
 
     this.backLayer.on('mousedown', function(e) {
         that.addPolygonPoint(e.x, e.y);
     });
+
+    this.backLayer.on('mousemove', function(e) {
+        if (!this.shape) return;
+        if (this.shape.type == 'polygon') this.updatePolygonShape(e.x, e.y);
+        if (this.shape.type == 'circle') this.updateCircleShape(e.x, e.y);
+    });
 };
 
 // -----------------------------------------------------------------------------
-// PUBLIC API
+// API for shape management
 // -----------------------------------------------------------------------------
 
 Canvas.prototype.addPolygonPoint = function(x, y) {
     var that = this;
 
+    // Create shape if needed.
+    if (this.shape == null) {
+        this.shape = {type: 'polygon', points: []};
+    }
+
+    // Update last line to end at new point.
+    if (that.shape.points.length > 0) {
+        that.updateLine(_.last(that.shape.points), x, y);
+        that.linesLayer.draw();
+    }
+
+    // Create a new line from this point.
+    var lineConfig = _.merge({points: [0, 0, 0, 0], listening: false}, Canvas.LineStyle);
+    var line = new Kinetic.Line(lineConfig);
+
+    this.linesLayer.add(line);
+    this.animateFadeIn(line);
+
+    // Create a point visual at this point.
     var pointConfig = _.merge({x: x, y: y, draggable: true}, Canvas.PointStyle);
     var point = new Kinetic.Circle(pointConfig);
     point.prevNode = _.last(this.shape.points);
     point.nextNode = null;
     point.prevLine = null;
-    point.nextLine = null;
+    point.nextLine = line;
 
     this.shape.points.push(point);
     this.pointsLayer.add(point);
     this.animateScaleIn(point);
 
-    point.on('dragmove', function(e) {
-        if (point.prevNode) {
-            var pos1 = point.prevNode.position();
-            var pos2 = point.position();
-            point.prevLine.points([pos1.x, pos1.y, pos2.x, pos2.y]);
-        }
-
-        if (point.nextNode) {
-            var pos1 = point.position();
-            var pos2 = point.nextNode.position();
-            point.nextLine.points([pos1.x, pos1.y, pos2.x, pos2.y]);
-        }
-
-        that.linesLayer.draw();
-    });
-
+    // If a previous sibling point exists, update it.
     if (point.prevNode) {
-        var pos = point.prevNode.position();
-        var lineConfig = _.merge({points: [pos.x, pos.y, x, y], listening: false}, Canvas.LineStyle);
-        var line = new Kinetic.Line(lineConfig);
-
-        this.linesLayer.add(line);
-        this.animateFadeIn(line);
-
         point.prevNode.nextNode = point;
-        point.prevNode.nextLine = line;
-        point.prevLine = line;
+        point.prevLine = point.prevNode.nextLine;
     }
 
+    // It this visual is the first one, register a click listener.
     if (!point.prevNode) {
-        point.on('mouseenter', function(e) {
-            if (that.shape.points.length < 3) return;
-
-            point.setAttrs(Canvas.PointOverStyle);
-            that.pointsLayer.draw();
-        });
-
-        point.on('mouseleave', function(e) {
-            point.setAttrs(Canvas.PointStyle);
-            that.pointsLayer.draw();
-        });
-
         point.on('click', function(e) {
             if (that.shape.points.length < 3) return;
 
             var lastPoint = _.last(that.shape.points);
-            
-            var pos1 = lastPoint.position();
-            var pos2 = point.position();
-            var lineConfig = _.merge({points: [pos1.x, pos1.y, pos2.x, pos2.y], listening: false}, Canvas.LineStyle);
-            var line = new Kinetic.Line(lineConfig);
-
-            that.linesLayer.add(line);
-            that.animateFadeIn(line);
 
             point.prevNode = lastPoint;
-            point.prevLine = line;
+            point.prevLine = lastPoint.nextLine;
             lastPoint.nextNode = point;
-            lastPoint.nextLine = line;
 
-            point.fire('mouseleave');
-            point.off('click mouseenter mouseleave');
+            point.off('click');
 
-            that.shape = {points: []};
+            that.shape = null;
         });
+    }
+
+    // Register some listeners for visual updates.
+    point.on('mouseenter', function(e) {
+        point.setAttrs(Canvas.PointOverStyle);
+        that.pointsLayer.draw();
+        that.updateLine(_.last(that.shape.points), point.position().x, point.position().y);
+        that.linesLayer.draw();
+    });
+
+    point.on('mouseleave', function(e) {
+        point.setAttrs(Canvas.PointStyle);
+        that.pointsLayer.draw();
+    });
+
+    point.on('dragmove', function(e) {
+        if (point.prevNode) that.updateLine(point.prevNode);
+        if (point.nextNode) that.updateLine(point);
+        that.linesLayer.draw();
+    });
+};
+
+Canvas.prototype.addCirclePoint = function(x, y) {
+    var that = this;
+
+    if (this.shape == null) {
+        this.shape = {type: 'circle', center: null, circle: null};
+
+        var circleConfig = _.merge({x: x, y: y, radius: 0, listening: false}, Canvas.CircleStyle);
+        var circle = new Kinetic.Circle(circleConfig);
+
+        this.shape.circle = circle;
+        this.linesLayer.add(circle);
+        this.linesLayer.draw();
+
+        var pointConfig = _.merge({x: x, y: y, draggable: true}, Canvas.PointStyle);
+        var point = new Kinetic.Circle(pointConfig);
+
+        this.shape.center = point;
+        this.pointsLayer.add(point);
+        this.animateScaleIn(point);
+
+    } else {
+        var dx = Math.abs(this.shape.center.position().x - x);
+        var dy = Math.abs(this.shape.center.position().y - y);
+        this.shape.circle.radius(Math.sqrt(dx*dx + dy*dy));
+        this.linesLayer.draw();
+
+        this.shape = null;
     }
 };
 
-// Canvas.prototype.addCirclePoint = function(x, y) {
-//     var shape = _.last(this.model.circleShapes);
+Canvas.prototype.updatePolygonShape = function(x, y) {
+    var point = _.last(this.shape.points);
+    if (!point) return;
 
-//     if (!shape || shape.isClosed) {
-//         shape = new Models.CircleShape();
-//         this.model.circleShapes.push(shape);
+    this.updateLine(point, x, y);
+    this.linesLayer.draw();
+};
 
-//         shape.center.x = x;
-//         shape.center.y = y;
-
-//         var pointConfig = _.merge({x: x, y: y}, Canvas.PointStyle);
-//         var point = new Kinetic.Circle(pointConfig);
-//         this.pointsLayer.add(point);
-//         this.pointsLayer.draw();
-
-//         var circleConfig = _.merge({x: x, y: y, radius: 20}, Canvas.CircleStyle);
-//         var circle = new Kinetic.Circle(circleConfig);
-//         this.linesLayer.add(circle);
-//         this.linesLayer.draw();
-
-//     } else {
-//         var dx = x - shape.center.x;
-//         var dy = y - shape.center.y;
-
-//         shape.radius = Math.sqrt(dx * dx + dy * dy);
-//         shape.isClosed = true;
-//     }
-// };
+Canvas.prototype.updateCircleShape = function(x, y) {
+    var dx = Math.abs(this.shape.center.position().x - x);
+    var dy = Math.abs(this.shape.center.position().y - y);
+    this.shape.circle.radius(Math.sqrt(dx*dx + dy*dy));
+    this.linesLayer.draw();
+};
 
 // -----------------------------------------------------------------------------
 // HELPERS
 // -----------------------------------------------------------------------------
+
+Canvas.prototype.updateLine = function(point, x, y) {
+    var pos1 = point.position();
+    var pos2 = y ? {x: x, y: y} : point.nextNode.position();
+    point.nextLine.points([pos1.x, pos1.y, pos2.x, pos2.y]);
+};
 
 Canvas.prototype.animateScaleIn = function(obj) {
     obj.setAttrs({scaleX: 0, scaleY: 0});
